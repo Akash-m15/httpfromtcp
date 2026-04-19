c2406c7028e53ef988576d5abb1de291a03e1d4b
Akash-m15 <ashdragneel15@gmail.com>
2026-04-19 19:24:11 +0530

    feat: Implement http-body-parser

--- internal/request/request.go
+++ internal/request/request.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 	"unicode"
 
@@ -16,6 +17,7 @@ const BUFFER_SIZE = 1024
 type Request struct {
 	RequestLine RequestLine
 	Headers     headers.Headers
+	Body        []byte
 	State       ParseState
 }
 
@@ -25,6 +27,7 @@ const (
 	StateInit ParseState = iota
 	StateInitialized
 	StateHeaders
+	StateBody
 	StateDone
 )
 
@@ -38,14 +41,27 @@ func newRequest() *Request {
 	return &Request{
 		State:   StateInitialized,
 		Headers: headers.NewHeaders(),
+		Body:    []byte{},
 	}
+}
+
+func getInt(headers headers.Headers, name string, defaultValue int) int {
+	valueStr, ok := headers.Get(name)
+	if !ok {
+		return defaultValue
+	}
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return defaultValue
+	}
+	return value
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	var parsedReq = newRequest()
 	buffer := make([]byte, BUFFER_SIZE)
 	bufIdx := 0
-
+	fmt.Println("Parse start")
 	for parsedReq.State != StateDone {
 		if len(buffer) <= bufIdx {
 			newBuffer := make([]byte, 2*len(buffer))
@@ -57,8 +73,14 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		bytesRead, err := reader.Read(buffer[bufIdx:])
 		if err != nil {
 			if errors.Is(err, io.EOF) {
+				fmt.Println("Hitting EOF", bufIdx, parsedReq.State)
+
 				// EOF hit: try to parse whatever is left in the buffer one last time
-				if bufIdx > 0 {
+				for bufIdx >= 0 {
+					// If parsing completed the request, we're good
+					if parsedReq.State == StateDone {
+						return parsedReq, nil
+					}
 					parsed, parseErr := parsedReq.parse(buffer[:bufIdx])
 					if parseErr != nil {
 						return nil, parseErr
@@ -69,14 +91,7 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 						bufIdx -= parsed
 					}
 				}
-
-				// If parsing completed the request, we're good
-				if parsedReq.State == StateDone {
-					break
-				}
-
 				// Otherwise, the request was incomplete when connection closed
-				return nil, io.ErrUnexpectedEOF
 			}
 			return nil, err
 		}
@@ -93,11 +108,13 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		bufIdx -= bytesParsed
 
 	}
+	fmt.Println("Parse end")
+	fmt.Println("--------------")
 	return parsedReq, nil
 }
 
 func (r *Request) parse(data []byte) (int, error) {
-
+	fmt.Println("Inside Parse", string(data))
 	switch r.State {
 	case StateInitialized:
 		req, bytesParsed, err := parseRequestLine(data)
@@ -109,25 +126,50 @@ func (r *Request) parse(data []byte) (int, error) {
 		}
 		r.RequestLine = req.RequestLine
 		r.State = StateHeaders
+		fmt.Println("Returning from StateInit: ", bytesParsed)
 		return bytesParsed, nil
 
 	case StateHeaders:
-
 		bytesRead, done, err := r.Headers.Parse(data)
-
 		if err != nil {
 			return 0, err
 		}
 		if done {
-			r.State = StateDone
+			r.State = StateBody
+			fmt.Println("Returning from StateHeaders (2): ", bytesRead)
 			return bytesRead, nil
 		} else {
+			fmt.Println("Returning from StateHeaders (1): ", bytesRead)
 			return bytesRead, nil
 		}
 
+	case StateBody:
+		fmt.Println("Enter StateBody")
+		length := getInt(r.Headers, "content-length", 0)
+		if length == 0 {
+			r.State = StateDone
+			fmt.Println("Returning from StateBody: (length = 0) ")
+			return 0, nil
+		}
+
+		remaining := min(length-len(r.Body), len(data))
+		r.Body = append(r.Body, data[:remaining]...)
+
+		if len(r.Body) == length {
+			r.State = StateDone
+			fmt.Println("Returning from StateBody: (length = body)", remaining)
+			return remaining, nil
+		}
+		if len(r.Body) > length {
+			return 0, fmt.Errorf("Body greater than content length")
+		}
+		fmt.Println("Returning from StateBody: (1) ", remaining)
+		return remaining, nil
 	case StateDone:
+		fmt.Println("Entering StateDone in Parse func")
 		return 0, fmt.Errorf("error: trying to read data  in a done state")
 	}
+
 	return 0, fmt.Errorf("error: Unknown State")
 }
 
